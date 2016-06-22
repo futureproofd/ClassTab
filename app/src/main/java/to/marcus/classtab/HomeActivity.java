a56200d5d04ec76fde6f5b5febaa85ca75e7401f
Marcus <nexus06@gmail.com>
2016-06-22 16:54:37 -0400

    Work in progress: Web parser working via traverse through A-Z index and create unique keys

--- app/src/main/java/to/marcus/classtab/HomeActivity.java
+++ app/src/main/java/to/marcus/classtab/HomeActivity.java
@@ -2,17 +2,12 @@ package to.marcus.classtab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
-import org.jsoup.select.NodeVisitor;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -25,9 +20,6 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        //open local index.htm for parsing
-        //notification service will determine when a new version is available from classtab
-            //then replace file and rebuild
         InputStream inputStream = null;
         Document document = null;
         String elementHolder = null;
@@ -40,54 +32,59 @@ public class HomeActivity extends AppCompatActivity {
             inputStream = this.getAssets().open("index.htm");
             document = Jsoup.parse(inputStream, "UTF-8", "http://placeholder.com");
             Elements elementDiv = document.select(ARTISTS_INDEX);
-            //get artist div
-            for(Element e : elementDiv.select("div[class=artist_a")){
-                //get artist list
-                for(Element artist : e.select("ul")){
-                    for(Element title : artist.select("b")){
-                        System.out.println(title.text());
-                        artistMap.put("a"+ String.valueOf(count),title.text());
-                        break;
-                    }
-
-                    while(count < artistMap.size()){
-                        for(Element songData : artist.getAllElements()){
-                            Elements links = songData.getAllElements().select("a[href]");
-                            if(links.size()==0){
-                                break;
-                            }else{
-                                for(int i=0; i<links.size();i++){
-                                    //todo: put this id in correct place to associate records
-                                    String idKey = generateUID("a",count);
-                                    if(links.get(i).toString().contains(".txt")){
-                                        //idKey = generateUID("a",count);
-                                        songMap.put(idKey, links.get(i).attr("href"));
-                                    }else if(links.get(i).toString().contains("MIDI")) {
-                                       // idKey = generateUID("a",count);
-                                        midiMap.put(idKey, links.get(i).attr("href"));
-                                    }else if(links.get(i).toString().contains("youtube.com")){
-                                       // idKey = generateUID("a",count);
-                                        vidMap.put(idKey, links.get(i).attr("href"));
-                                    }
-                                }
-                                count++;
-                            }
-                        }
-                    }
-                }
-                String stop = "stop";
-                //14 artists
-                //84 songs
-                //51 midis
-                //45 vids
-            }
+            //get artist index A-Z
+           for(char indexLetter = 'a'; indexLetter <= 'b'; indexLetter++) {
+               //get artist div
+               for (Element e : elementDiv.select("div[class=artist_"+indexLetter)) {
+                   //get artist list
+                   for (Element artist : e.select("ul")) {
+                       for (Element title : artist.select("b")) {
+                           System.out.println(title.text());
+                           artistMap.put(indexLetter + String.valueOf(count), title.text());
+                           break;
+                       }
+                       while (count < artistMap.size()) {
+                           for (Element songData : artist.getAllElements()) {
+                               Elements links = songData.getAllElements().select("a[href]");
+                               if (links.size() == 0) {
+                                   break;
+                               } else {
+                                   String recordType = "song";
+                                   String idKey = "";
+                                   for (int i = 0; i < links.size(); i++) {
+                                       //todo: put id in correct place to associate records
+                                       switch (recordType) {
+                                           case "song":
+                                               idKey = generateUID(indexLetter, count);
+                                               break;
+                                       }
+                                       if (links.get(i).toString().contains(".txt")) {
+                                           if (recordType != "song") {
+                                               idKey = generateUID(indexLetter, count);
+                                           }
+                                           songMap.put(idKey, links.get(i).attr("href"));
+                                           recordType = "";
+                                       } else if (links.get(i).toString().contains("MIDI")) {
+                                           midiMap.put(idKey, links.get(i).attr("href"));
+                                       } else if (links.get(i).toString().contains("youtube.com")) {
+                                           vidMap.put(idKey, links.get(i).attr("href"));
+                                       }
+                                   }
+                                   count++;
+                               }
+                           }
+                       }
+                   }
+               } //end indexLetter
+           }
         }catch(IOException exception){
             exception.printStackTrace();
         }
+        String stop = "stop";
     }
 
-    private static String generateUID(String alphaIndex, int recordPtr){
+    private static String generateUID(char alphaIndex, int recordPtr){
         Random rand = new Random();
-        return alphaIndex+String.valueOf(recordPtr)+"-"+ String.valueOf(rand.nextInt());
+        return Character.toString(alphaIndex)+String.valueOf(recordPtr)+"-"+ String.valueOf(rand.nextInt());
     }
 }
