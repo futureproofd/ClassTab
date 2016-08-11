1292243c15aef61732b1775085d60b1adcc80ee7
Marcus <nexus06@gmail.com>
2016-08-11 16:28:58 -0400

    Added Multiple ViewHolder support for columnspan custom styling
    -ArtistAdapter supports multiple viewHolder binding to determine styling of top artist (based on accessTime)

--- app/src/main/java/to/marcus/classtab/ui/ArtistAdapter.java
+++ app/src/main/java/to/marcus/classtab/ui/ArtistAdapter.java
@@ -27,8 +27,11 @@ import to.marcus.classtab.data.model.ArtistSorter;
 /**
  * Created by marcus on 7/8/2016
  */
-public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistsViewHolder>{
+public class ArtistAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
+    private static final int VIEW_BIG_ARTIST = 0;
+    private static final int VIEW_NORMAL_ARTIST = 1;
+    private static final int VIEW_LOADING = -1;
     private LinkedHashMap<Integer,Artist> mArtists;
     private final RecyclerViewArtistClickListener clickListener;
     private final int mNumColumns;
@@ -42,10 +45,37 @@ public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistsVie
     }
 
     @Override
-    public ArtistAdapter.ArtistsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case VIEW_BIG_ARTIST:
+                return createBigArtistViewHolder(parent);
+            case VIEW_NORMAL_ARTIST:
+                return createNormalArtistViewHolder(parent);
+            case VIEW_LOADING:
+                //return new LoadingViewHolder(
+                        //inflate
+              //  );
+        }
+        return null;
+        }
+
+    private NormalArtistViewHolder createNormalArtistViewHolder(ViewGroup parent) {
         final View artistView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_artist,parent,false);
-        final ArtistsViewHolder artistsViewHolder = new ArtistsViewHolder(artistView);
+        final NormalArtistViewHolder artistsViewHolder = new NormalArtistViewHolder(artistView);
+        artistView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                clickListener.onObjectClick(v,mArtists.get(artistsViewHolder.getAdapterPosition()).getId());
+            }
+        });
+        return artistsViewHolder;
+    }
+
+    private BigArtistViewHolder createBigArtistViewHolder(ViewGroup parent) {
+        final View artistView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_big_artist,parent,false);
+        final BigArtistViewHolder artistsViewHolder = new BigArtistViewHolder(artistView);
         artistView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -56,7 +86,30 @@ public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistsVie
     }
 
     @Override
-    public void onBindViewHolder(ArtistAdapter.ArtistsViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        switch (getItemViewType(position)) {
+            case VIEW_BIG_ARTIST:
+                bindBigArtistView((BigArtistViewHolder)holder, position); //setText etc
+                break;
+            case VIEW_NORMAL_ARTIST:
+                bindNormalArtistView((NormalArtistViewHolder)holder,position);
+                break;
+            case VIEW_LOADING:
+                //bindLoadingView;
+                break;
+        }
+    }
+
+    private void bindBigArtistView(final BigArtistViewHolder holder, int position){
+        holder.artistBigNameView.setText(mArtists.get(position).getName());
+        holder.artistBigDateView.setText(mArtists.get(position).getDate());
+        Context context = holder.artistBigImageView.getContext();
+        Picasso.with(context).load(mArtists.get(position).getUrl())
+                .placeholder(R.drawable.user96)
+                .into(holder.artistBigImageView);
+    }
+
+    private void bindNormalArtistView(final NormalArtistViewHolder holder, int position){
         holder.artistNameView.setText(mArtists.get(position).getName());
         holder.artistDateView.setText(mArtists.get(position).getDate());
         Context context = holder.artistImageView.getContext();
@@ -70,22 +123,23 @@ public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistsVie
         return mArtists.size();
     }
 
+    @Override
+    public int getItemViewType(int position){
+        if(mArtists.get(position).getColumnSpan() == 2){
+            return VIEW_BIG_ARTIST;
+        }else if(position > 0){
+            return VIEW_NORMAL_ARTIST;
+        }else{
+            return VIEW_LOADING;
+        }
+    }
+
     /*
     Main entry point from Activity, to set the list of artists
      */
     public void setArtists(LinkedHashMap<Integer,Artist> artists){
         sortArtists(artists);
         resizePopularArtist();
-    }
-
-    class ArtistsViewHolder extends RecyclerView.ViewHolder{
-        @BindView(R.id.txt_artist_name)TextView artistNameView;
-        @BindView(R.id.txt_artist_date) TextView artistDateView;
-        @BindView(R.id.img_artist) ImageView artistImageView;
-        public ArtistsViewHolder(View view){
-            super(view);
-            ButterKnife.bind(this, view);
-        }
     }
 
     //After sorting, the Latest accessed record should always be first
@@ -102,5 +156,24 @@ public class ArtistAdapter extends RecyclerView.Adapter<ArtistAdapter.ArtistsVie
         notifyDataSetChanged();
     }
 
+    class NormalArtistViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.txt_artist_name)TextView artistNameView;
+        @BindView(R.id.txt_artist_date) TextView artistDateView;
+        @BindView(R.id.img_artist) ImageView artistImageView;
+        public NormalArtistViewHolder(View view){
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    class BigArtistViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.txt_big_artist_name)TextView artistBigNameView;
+        @BindView(R.id.txt_big_artist_date)TextView artistBigDateView;
+        @BindView(R.id.img_big_artist) ImageView artistBigImageView;
+        public BigArtistViewHolder(View view){
+            super(view);
+            ButterKnife.bind(this,view);
+        }
+    }
 
 }
